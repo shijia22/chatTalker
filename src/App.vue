694ccbd0ff847e4468cfee57bbc45fed4c3728db
shijia22 <shichia0207@gmail.com>
2022-05-31 10:31:15 +0800

    feat: 🎸 add home top

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Header />
+    <Header class="relative z-10"/>
   <section class="container py-8 flex flex-col">
     <div class="row flex-col">
       <div class="col md:grid md:grid-cols-2 md:items-center md:gap-[80px]">
-        <div>
+        <div class=" relative z-10">
           <h2 class="mb-2 text-neutral leading-6 font-bold md:text-h3">ChatTalker 最全面的聊天機器人</h2>
           <span class="mb-4 text-h1 md:text-5xl md:leading-[4.375rem] leading-h1 font-bold">你愛我像誰？<br />扮演什麼角色我都會</span>
           <h3 class="mb-8  md:mb-0 md:mt-4 text-content md:text-[1.25rem] leading-content">
@@ -13,8 +13,11 @@
         <div class="flex justify-center md:justify-end">
           <img src="/svg/bg_1.svg" alt="bg" class="w-full md:hidden">
           <div class="absolute md:flex md:static">
-            <div class="relative top-0">
-              <img src="/svg/bg_1_1.svg" alt="person" class="w-[312px] z-10">
+            <div class="relative top-0 z-10">
+              <img src="/svg/bg_1_1.svg" alt="person" class="w-[312px] md:w-[532px]">
+            </div>
+            <div class="absolute top-0 right-0 hidden md:flex">
+              <img src="/svg/bg_home.svg" alt="bg" class="">
             </div>
           </div>
         </div>
--- src/components/templates/Header.vue
+++ src/components/templates/Header.vue
@@ -1,10 +1,12 @@
 <template>
   <nav class="py-6 border-b-2 border-b-primary-500 md:border-0">
-    <div class="container flex items-center justify-between">
-      <h1>
-        <a href="#"><img src="/logo.png" alt="LOGO" class="h-6"></a>
-      </h1>
-      <button type="button" class="px-8 py-2 rounded-[22px] bg-secondary text-grey-100 text-content">方案費用</button>
+    <div class="container">
+      <div class="row items-center justify-between">
+        <h1>
+          <a href="#"><img src="/logo.png" alt="LOGO" class="h-6"></a>
+        </h1>
+        <button type="button" class="px-8 py-2 rounded-[22px] bg-secondary text-grey-100 text-content">方案費用</button>
+      </div>
     </div>
   </nav>
 </template>
