a2203fae303d03513c17f9f608379659dc4f37a2
shijia22 <shichia0207@gmail.com>
2022-05-31 17:19:56 +0800

    feat: 🎸 Footer

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
   <Platforms />
   <Customer />
   <Feature />
+  <Footer />
 </template>
 
 <script setup>
 import Header from '@/components/templates/Header.vue'
+import Footer from '@/components/templates/Footer.vue';
 import Intro from '@/components/Intro.vue';
 import Platforms from '@/components/Platforms.vue';
 import Customer from '@/components/Customer.vue';
---
+++ src/components/templates/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-grey-200">
+    <div class="container">
+      <p class="mx-6 py-4 text-center leading-6">© 2022 chatTalker. All Rights Reserved.</p>
+    </div>
+  </footer>
+</template>
