77447d4e202c938728e3452cc92c26b13f7f7db7
shijia22 <shichia0207@gmail.com>
2022-06-01 11:49:12 +0800

    fix: 🐛 section 6 mt

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -10,8 +10,8 @@
   <section class="mt-[6.25rem] bg-[url('/svg/home_footer.svg')] hidden xs:flex bg-contain bg-no-repeat bg-center">
     <div class="container">
       <div class="row justify-center">
-        <div class="col mb-[3.75rem] mt-[320px] text-center">
-          <h3 class="mb-6 text-2xl font-bold">\ 美好用戶體驗，從對話開始 /</h3>
+        <div class="col mb-[3.75rem] text-center">
+          <h3 class="mb-6 mt-[320px] md:mt-[200px] text-2xl font-bold">\ 美好用戶體驗，從對話開始 /</h3>
           <button type="button"
             class="px-8 py-2 mb-8 rounded-[22px] bg-neutral text-grey-100 text-content">方案費用</button>
         </div>
