d12180fb607a069720500659b792b6d41119eeb2
shijia22 <shichia0207@gmail.com>
2022-06-01 15:00:55 +0800

    style: 💄 調整空格

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,10 +2,10 @@
   <section class="container py-8 flex flex-col">
     <div class="row flex-col">
       <div class="col md:grid md:grid-cols-2 md:items-center md:gap-[80px]">
-        <div class=" relative z-10">
+        <div class="relative z-10">
           <h2 class="mb-2 text-neutral leading-6 font-bold md:text-h3">ChatTalker 最全面的聊天機器人</h2>
           <span class="mb-4 text-h1 md:text-5xl md:leading-[4.375rem] leading-h1 font-bold">你愛我像誰？<br />扮演什麼角色我都會</span>
-          <h3 class="mb-8  md:mb-0 md:mt-4 text-content md:text-[1.25rem] leading-content">
+          <h3 class="mb-8 md:mb-0 md:mt-4 text-content md:text-[1.25rem] leading-content">
             Facebook、Instagram、LINE 聊天機器人解決方案
           </h3>
         </div>
